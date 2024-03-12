857bdd84e93eff059d25922853070cd95000e25e
mayday <antoshakarpyuk@gmail.com>
2024-03-12 13:05:48 +0200

    new project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IPZ_CW_2_Karpyuk_Anton"
+include(":app")
+ 
