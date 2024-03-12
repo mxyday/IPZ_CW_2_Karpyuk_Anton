ed5284ed8fcac343b955853d1d59c40cdbbeb190
mayday <antoshakarpyuk@gmail.com>
2024-03-12 14:19:00 +0200

    basic form

--- app/src/main/java/ua/edu/lntu/ipz_cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_2/MainActivity.kt
@@ -3,12 +3,16 @@ package ua.edu.lntu.ipz_cw_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.ipz_cw_2.ui.theme.IPZ_CW_2_Karpyuk_AntonTheme
 
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SignInCard("")
                 }
             }
         }
@@ -30,17 +34,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SignInCard(name: String, modifier: Modifier = Modifier) {
+    Column(
+        var signInState by remember { mutableStateOf<SignInState>(SignInState.SignIn) }
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+
+        modifier = Modifier
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = { onEmailChange(it) },
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { onPasswordChange(it) },
+            )
+        )
+        Button(onClick = { onSignInClick() }) {
+            Text("Sign In")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SignInPreview() {
     IPZ_CW_2_Karpyuk_AntonTheme {
-        Greeting("Android")
+        SignInCard("")
     }
 }
