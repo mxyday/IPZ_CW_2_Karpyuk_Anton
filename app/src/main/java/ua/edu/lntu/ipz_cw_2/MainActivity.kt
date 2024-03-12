011b670cc0bd698e2b695e024f62618d9ff7751f
mayday <antoshakarpyuk@gmail.com>
2024-03-12 14:59:39 +0200

    Stylezing elements

--- app/src/main/java/ua/edu/lntu/ipz_cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_2/MainActivity.kt
@@ -3,8 +3,11 @@ package ua.edu.lntu.ipz_cw_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -15,10 +18,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.ipz_cw_2.ui.theme.IPZ_CW_2_Karpyuk_AntonTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,16 +44,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SignInCard(name: String, modifier: Modifier = Modifier) {
+fun SignInCard(
+    name: String,
+    modifier: Modifier = Modifier
+
+) {
     var signInTrue by remember { mutableStateOf(false) }
     var email by remember { mutableStateOf(TextFieldValue()) }
     var password by remember { mutableStateOf(TextFieldValue()) }
 
+
     if (!signInTrue) {
         Column(
-            modifier = Modifier
+            modifier = Modifier.fillMaxWidth().padding(top = 150.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             OutlinedTextField(
+                modifier = Modifier.padding(5.dp),
                 value = email,
                 onValueChange = { email = it },
             )
@@ -66,15 +79,21 @@ fun SignInCard(name: String, modifier: Modifier = Modifier) {
         }
     }
     else {
-        Text(
-            text = "Sign in successful"
-        )
-        Button(onClick = {
-            signInTrue = false
-            email = TextFieldValue()
-            password = TextFieldValue()
-        }) {
-            Text("Sign Out")
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(top = 150.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Sign in successful"
+            )
+            Button(onClick = {
+                signInTrue = false
+                email = TextFieldValue()
+                password = TextFieldValue()
+            }) {
+                Text("Sign Out")
+            }
         }
     }
 }
